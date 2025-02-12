dcbdd6aaf1cecb0e629243a590f70a51bb584601
Sid <contact@sidali.dev>
2025-02-12 16:44:24 +0100

    vercel

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.get('/', (req, res) => res.send('Express on Vercel'))
 
 // Routes
-app.use('/todos', require('./src/routes/todos'))
+app.use('/todos', require('../src/routes/todos'))
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
